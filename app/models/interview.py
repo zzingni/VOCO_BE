4a1635cce4852885bf10c5cb1a2f27f38ab00aee
zzingni <wldms001313@gmail.com>
2026-05-11 14:56:49 +0900

    feat : model 외래관계 수정

---
+++ app/models/interview.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, ForeignKey
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+from app.db.database import Base
+
+
+class Interview(Base):
+    __tablename__ = "interview"
+
+    id = Column(Integer, primary_key=True, index=True)
+    interview_date = Column(TIMESTAMP, server_default=func.now())
+
+    # Answer 와 1:N 관계
+    answers = relationship("Answer", back_populates="interview")
