df782aee83d44c0f20eb7ac09ee254bf60c12bdb
zzingni <wldms001313@gmail.com>
2026-05-18 10:02:47 +0900

    feat : tag model 생성

---
+++ app/models/tag.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Text, Boolean, ForeignKey, TIMESTAMP
+from sqlalchemy.orm import relationship
+from app.db.database import Base
+
+class Tag(Base):
+
+    __tablename__ = "tag"
+
+    tag_id = Column(Integer, primary_key=True, index=True)
+    tag_name = Column(String(100), nullable=False)
+    description = Column(String(255), nullable=False)
+
+    status =  Column(String(20), default="ACTIVE")
+
+    posts = relationship("Post", back_populates="tag")
